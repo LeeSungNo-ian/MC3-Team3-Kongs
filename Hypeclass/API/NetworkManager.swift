665fdf0bf42128840b2071c0b3c72192a493695e
LeeSungNo-ian <isnn0103@gmail.com>
2022-07-28 03:24:23 +0900

    [FEAT] Network Error 정의

---
+++ Hypeclass/API/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  Hypeclass
+//
+//  Created by 이성노 on 2022/07/28.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case networkingError
+    case dataError
+    case parseError
+}
