caa35b589db66d079cdf485ae810962e728ab101
LeeSungNo-ian <isnn0103@gmail.com>
2022-07-28 03:44:56 +0900

    [FEAT] Constant requestURL 구현

--- Hypeclass/API/Constant.swift
+++ Hypeclass/API/Constant.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
-class Constant {
-    
+
+public enum YouTubeAPI {
+    static let requestURL = "https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&key="
 }
+
+
+
+
+
+
+
+
+    // https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&maxResults=50&key=AIzaSyCbHjgfcmHGhJB-yeALi7rSvtPu3vaCy2o
+
+    // https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&key=AIzaSyCbHjgfcmHGhJB-yeALi7rSvtPu3vaCy2o
---
+++ Hypeclass/Config/Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  Hypeclass
+//
+//  Created by 이성노 on 2022/07/28.
+//
+
+import Foundation
+
+//public enum YouTubeAPI {
+//    static let requestURL = "https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&key="
+//}
+
+
+
+
+
+
+
+
+
+// https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&maxResults=50&key=AIzaSyCbHjgfcmHGhJB-yeALi7rSvtPu3vaCy2o
+
+// https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&key=AIzaSyCbHjgfcmHGhJB-yeALi7rSvtPu3vaCy2o
