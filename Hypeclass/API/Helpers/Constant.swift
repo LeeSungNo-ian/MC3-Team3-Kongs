0d2a0f0579858231df3e95c175d357f28b4fb92c
LeeSungNo-ian <isnn0103@gmail.com>
2022-08-01 01:36:00 +0900

    [REFACTOR] 사용하지 않는 행간 제거

--- Hypeclass/API/Helpers/Constant.swift
+++ Hypeclass/API/Helpers/Constant.swift
@@ -11,14 +11,3 @@ import Foundation
 public enum YouTubeAPI {
     static let requestURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCx-DjQ3m3Al6oFKZHmycO9w&maxResults=5&order=date&key="
 }
-
-
-
-
-
-
-
-
-    // https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&maxResults=50&key=AIzaSyCbHjgfcmHGhJB-yeALi7rSvtPu3vaCy2o
-
-    // https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&channelId=UCsNfSIKbJ9QBAegU-2BnlXA&key=AIzaSyCbHjgfcmHGhJB-yeALi7rSvtPu3vaCy2o
